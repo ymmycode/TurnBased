e7840260576c671f3442c6e7dbb6f1cc40fa6c99
Achmat Fauzi <achmatfauzi86@gmail.com>
2020-04-04 05:09:09 +0700

    Adding User Interfaces to Scene

---
+++ TurnBased/Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseEnemy
+{
+    public string name;
+
+    public enum Type
+    { 
+        GRASS,
+        FIRE,
+        WATER,
+        ELECTRIC
+    };
+
+    public enum Rarity
+    {
+        COMMON,
+        UNCOMMON,
+        RARE,
+        SUPERRARE
+    };
+
+
+    public Type enemyType;
+    public Rarity rarity;
+
+    public float baseHP;
+    public float currentHP;
+
+    public float baseMP;
+    public float currentMP;
+
+    public float baseATK;
+    public float currentATK;
+    public float baseDEF;
+    public float currentDEF;
+
+
+}
---
+++ TurnBased/Assets/Scripts/BaseHero.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseHero 
+{
+    public string name;
+
+    public float baseHP;
+    public float currentHP;
+
+    public float baseMP;
+    public float currentMP;
+
+    public int stamina;
+    public int intellect;
+    public int dexterity;
+    public int agility;
+
+
+
+}
